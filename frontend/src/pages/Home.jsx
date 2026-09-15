f024f31bfa42ae4e9f6c36a911467e3d38b7d8de
IcySoftware415 <isaacgpadilla07@gmail.com>
2026-09-15 16:06:53 -0600

    Add project name and description

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,13 @@ function Home() {
     return (
         <main className="page">
             <div className="page-content">
-                <h1 className="page-title">Project Name</h1>
+                <h1 className="page-title">Skill Up</h1>
 
-                <p className="page-description">A one-sentence description of the business problem. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p className="page-description">
+                    SkillUp helps you track your progress on whatever you want to learn. Whether that's everyday hobbies like cooking and reading, or career goals like learning new tools and earning certifications, SkillUp keeps you motivated and on track.
+                </p>
             </div>
         </main>
-        
     );
 }
 export default Home;
